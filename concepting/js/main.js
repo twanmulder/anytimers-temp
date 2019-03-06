9b81e7630e25547010c3e42e1c8c3a413ac40492
jengelstengel <twan.mulder@gmail.com>
2019-03-06 21:22:24 +0100

    Added shitty value increase
    Added shitty value increase

--- concepting/js/main.js
+++ concepting/js/main.js
@@ -1,3 +1,4 @@
+// Opening the drink buttons
 function on() {
   document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
   document.getElementById("add-bttn").style.margin = "26rem 2.5rem 0 12.5rem";
@@ -6,6 +7,7 @@ function on() {
   document.getElementById("beer-bttn").style.display = "none";
 }
 
+// Closing the drink buttons
 function off() {
   document.getElementById("overlay").style.backgroundColor = "transparent";
   document.getElementById("add-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
@@ -14,10 +16,12 @@ function off() {
   document.getElementById("beer-bttn").style.display = "block";
 }
 
+// Opening the add page
 function testadd() {
   document.getElementById("testadd").style.display = "block";
 }
 
+// Closing the add page + button update
 function testaddoff() {
   document.getElementById("testadd").style.display = "none";
   document.getElementById("overlay").style.backgroundColor = "transparent";
@@ -27,10 +31,12 @@ function testaddoff() {
   document.getElementById("beer-bttn").style.display = "block";
 }
 
+// Opening the drink page
 function testdrink() {
   document.getElementById("testdrink").style.display = "block";
 }
 
+// Closing the drink page + button update
 function testdrinkoff() {
   document.getElementById("testdrink").style.display = "none";
   document.getElementById("overlay").style.backgroundColor = "transparent";
@@ -38,4 +44,37 @@ function testdrinkoff() {
   document.getElementById("drink-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
   document.getElementById("drink-bttn").style.backgroundColor = "#31b16c";
   document.getElementById("beer-bttn").style.display = "block";
+}
+
+
+// Increasing add value
+function increaseAddValue() {
+  var score = parseInt(document.getElementById('addValue').innerHTML);
+    score = score+1;
+    document.getElementById('addValue').innerHTML = score;
+}
+// Decrease add value
+function decreaseAddValue() {
+  var score = parseInt(document.getElementById('addValue').innerHTML);
+  //check if score is not below zero
+  if (score >= 1) {
+    score = score-1;
+    document.getElementById('addValue').innerHTML = score;
+  }
+}
+
+// Increasing drink value
+function increaseDrinkValue() {
+  var score = parseInt(document.getElementById('drinkValue').innerHTML);
+    score = score+1;
+    document.getElementById('drinkValue').innerHTML = score;
+}
+// Decrease drink value
+function decreaseDrinkValue() {
+  var score = parseInt(document.getElementById('drinkValue').innerHTML);
+  //check if score is not below zero
+  if (score >= 1) {
+    score = score-1;
+    document.getElementById('drinkValue').innerHTML = score;
+  }
 }
