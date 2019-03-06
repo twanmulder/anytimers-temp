b856862332a27cbcf73566b0b18e45d879b723cf
jengelstengel <twan.mulder@gmail.com>
2019-03-06 19:47:21 +0100

    Updated js
    Updated js

--- concepting/js/main.js
+++ concepting/js/main.js
@@ -1,7 +1,13 @@
 function on() {
   document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  document.getElementById("add-bttn").style.margin = "26rem 2.5rem 0 12.5rem";
+  document.getElementById("beer-bttn").style.margin = "29.5rem 2.5rem 0 10rem";
+  document.getElementById("beer-bttn").style.backgroundColor = "rgb(255, 91, 91)";
 }
 
 function off() {
   document.getElementById("overlay").style.backgroundColor = "transparent";
+  document.getElementById("add-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
+  document.getElementById("beer-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
+  document.getElementById("beer-bttn").style.backgroundColor = "#31b16c";
 }
