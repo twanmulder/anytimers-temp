fc36aaf383b7e24c3b4dec0d55b3d69bb7e1b187
jengelstengel <twan.mulder@gmail.com>
2019-03-06 20:51:38 +0100

    Shitty js for testing purposes
    Shitty js for testing purposes

--- concepting/js/main.js
+++ concepting/js/main.js
@@ -1,13 +1,41 @@
 function on() {
   document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
   document.getElementById("add-bttn").style.margin = "26rem 2.5rem 0 12.5rem";
-  document.getElementById("beer-bttn").style.margin = "29.5rem 2.5rem 0 10rem";
-  document.getElementById("beer-bttn").style.backgroundColor = "rgb(255, 91, 91)";
+  document.getElementById("drink-bttn").style.margin = "29.5rem 2.5rem 0 10rem";
+  document.getElementById("drink-bttn").style.backgroundColor = "rgb(255, 91, 91)";
+  document.getElementById("beer-bttn").style.display = "none";
 }
 
 function off() {
   document.getElementById("overlay").style.backgroundColor = "transparent";
   document.getElementById("add-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
-  document.getElementById("beer-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
-  document.getElementById("beer-bttn").style.backgroundColor = "#31b16c";
+  document.getElementById("drink-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
+  document.getElementById("drink-bttn").style.backgroundColor = "#31b16c";
+  document.getElementById("beer-bttn").style.display = "block";
+}
+
+function testadd() {
+  document.getElementById("testadd").style.display = "block";
+}
+
+function testaddoff() {
+  document.getElementById("testadd").style.display = "none";
+  document.getElementById("overlay").style.backgroundColor = "transparent";
+  document.getElementById("add-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
+  document.getElementById("drink-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
+  document.getElementById("drink-bttn").style.backgroundColor = "#31b16c";
+  document.getElementById("beer-bttn").style.display = "block";
+}
+
+function testdrink() {
+  document.getElementById("testdrink").style.display = "block";
+}
+
+function testdrinkoff() {
+  document.getElementById("testdrink").style.display = "none";
+  document.getElementById("overlay").style.backgroundColor = "transparent";
+  document.getElementById("add-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
+  document.getElementById("drink-bttn").style.margin = "30rem 2.5rem 0 14.5rem";
+  document.getElementById("drink-bttn").style.backgroundColor = "#31b16c";
+  document.getElementById("beer-bttn").style.display = "block";
 }
