c06a39b90bc4e4a0ddcecbc379285e909b52d42f
jengelstengel <twan.mulder@gmail.com>
2019-03-06 19:32:42 +0100

    Started working on js to update main page
    Started working on js to update main page

---
+++ concepting/js/main.js
@@ -0,0 +1,7 @@
+function on() {
+  document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+}
+
+function off() {
+  document.getElementById("overlay").style.backgroundColor = "transparent";
+}
