f4f86df84580cf2149e3e809a7811d7dcab4dc28
TwanMulder <twan.mulder@gmail.com>
2019-12-16 21:56:06 +0100

    NEW WEBSITE!

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-(function () {
+;(function() {
   const win = window
   const doc = document.documentElement
 
@@ -12,31 +12,31 @@
   win.addEventListener('load', fixHeroBgHeight)
   win.addEventListener('resize', fixHeroBgHeight)
 
-  function fixHeroBgHeight () {
-    const bodyHeight = doc.getElementsByTagName('body')[0].clientHeight
+  function fixHeroBgHeight() {
+    const bodyHeight = doc.getElementsByTagName('body')[0].offsetHeight
     heroDesktopBg.style.height = `${bodyHeight}px`
   }
 
   // Reveal animations
   if (document.body.classList.contains('has-animations')) {
     /* global ScrollReveal */
-    const sr = window.sr = ScrollReveal()
+    const sr = (window.sr = ScrollReveal())
 
     sr.reveal('.hero-title, .hero-paragraph, .newsletter-form', {
       duration: 1000,
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'top',
-      interval: 150
+      interval: 150,
     })
 
     sr.reveal('.hero-ball', {
       delay: 1000,
-      duration: 1400,
+      duration: 2200,
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'bottom',
-      interval: 200
+      interval: 200,
     })
 
     sr.reveal('.hero-illustration-browser, .hero-squares', {
@@ -45,7 +45,16 @@
       distance: '40px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'right',
-      interval: 150
+      interval: 150,
+    })
+
+    sr.reveal('.anytimer-item, .anytimer-subtitle', {
+      delay: 800,
+      duration: 600,
+      distance: '40px',
+      easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
+      origin: 'bottom',
+      interval: 150,
     })
   }
-}())
+})()
